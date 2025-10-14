f8530905fb4d8273dc8e508b849709175250f6f0
이정현 <wjd1gus2@gmail.com>
2025-10-14 17:54:25 +0900

    feat(app): add userInput handler

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+
+const PLACEHOLDER = "덧셈할 문자열을 입력해 주세요.\n";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await userInput();
+    
+  }
+}
+
+async function userInput() {
+  return await Console.readLineAsync(PLACEHOLDER);
 }
 
 export default App;
