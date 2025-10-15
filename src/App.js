3fcf3692a3ffb445ad6cfff021172c97183d5462
이정현 <wjd1gus2@gmail.com>
2025-10-15 16:40:05 +0900

    feat(app): add error checking and message display

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import { Console } from "@woowacourse/mission-utils";
 
 const PLACEHOLDER = "덧셈할 문자열을 입력해 주세요.\n";
+const ERROR_MESSAGE = "[ERROR]";
 
 class App {
   async run() {
     const input = await userInput();
-    
+    const checkedtext = checkError(input);
+    if (!checkedtext) errorMessage();
+    Console.print(checkedtext);
   }
 }
 
 async function userInput() {
   return await Console.readLineAsync(PLACEHOLDER);
+}
+
+function checkError(text) {
+  const checkedtext =text.replace(/[^\d]/g, "");
+  return checkedtext ? true : false;
+}
+
+function errorMessage() {
+  throw new Error(ERROR_MESSAGE);
 }
 
 export default App;
